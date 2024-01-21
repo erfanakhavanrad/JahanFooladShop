b7864b59c3010608c29b102f32c91386334c0034
Erfan <erfanakhavanrad@outlook.com>
2024-01-21 11:44:49 +0330

    RealPerson CRUD written and tested.

--- src/main/java/com/jahanfoolad/jfs/controller/TestController.java
+++ src/main/java/com/jahanfoolad/jfs/controller/TestController.java
@@ -2,7 +2,6 @@ package com.jahanfoolad.jfs.controller;
 
 import com.jahanfoolad.jfs.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/jahanfoolad/jfs/domain/Test.java
+++ src/main/java/com/jahanfoolad/jfs/domain/Test.java
@@ -3,38 +3,26 @@ package com.jahanfoolad.jfs.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-public class Test {
-    @Id
-    @Column
-    private Long id;
+@Getter
+@Setter
+public class Test extends AbstractEntity {
+//    @Id
+//    @Column
+//    private Long id;
 
     @Column(name = "name")
     private String name;
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Test{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Test{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
 }
