3e8216257151a163924c92a07a8dab389a22c05c
hashemii87 <khadijeh.hashemi87@gmail.com>
2024-01-27 16:49:28 +0330

    CorpPerson added and Real Person trimmed.

--- src/main/java/com/jahanfoolad/jfs/JfsApplication.java
+++ src/main/java/com/jahanfoolad/jfs/JfsApplication.java
@@ -2,12 +2,40 @@ package com.jahanfoolad.jfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class JfsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JfsApplication.class, args);
+    }
+
+
+    @Configuration
+    public static class AppConfig{
+        @Bean
+        public MessageSource faMessageSource() {
+            ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+            messageSource.setBasename("message_fa");
+            messageSource.setDefaultLocale(Locale.ENGLISH);
+            messageSource.setDefaultEncoding("UTF-8");
+            return messageSource;
+        }
+
+        @Bean
+        public MessageSource enMessageSource() {
+            ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+            messageSource.setBasename("message");
+            messageSource.setDefaultLocale(Locale.ENGLISH);
+            messageSource.setDefaultEncoding("UTF-8");
+            return messageSource;
+        }
     }
 
 }
--- src/main/java/com/jahanfoolad/jfs/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/jahanfoolad/jfs/service/impl/CompanyServiceImpl.java
@@ -4,18 +4,26 @@ import com.jahanfoolad.jfs.domain.Company;
 import com.jahanfoolad.jfs.domain.dto.CompanyDto;
 import com.jahanfoolad.jfs.jpaRepository.CompanyRepository;
 import com.jahanfoolad.jfs.service.CompanyService;
+import jakarta.annotation.Resource;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class CompanyServiceImpl  implements CompanyService {
 
     @Autowired
     CompanyRepository  companyRepository;
+
+    @Resource(name = "faMessageSource")
+    private MessageSource faMessageSource;
+    @Resource(name = "enMessageSource")
+    private MessageSource enMessageSource;
 
     @Override
     public List<Company> getCompanyPersons() {
@@ -24,7 +32,7 @@ public class CompanyServiceImpl  implements CompanyService {
 
     @Override
     public Company getCompanyByUserId(Long id) throws Exception {
-        return companyRepository.findById(id).orElseThrow ( () -> new Exception("company not found"));
+        return companyRepository.findById(id).orElseThrow ( () -> new Exception(enMessageSource.getMessage("failed_message",null,Locale.ENGLISH)));
     }
 
     @Override
